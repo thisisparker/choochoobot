4110e4226d82128dc7a61aec6266807593cc7c71
Parker Higgins <parker@parkerhiggins.net>
2022-12-18 14:31:16 -0500

    add mastodon support (https://botsin.space/@choochoobot)

--- choochootweet.py
+++ choochootweet.py
@@ -4,7 +4,9 @@ import yaml, os
 import choochoogen
 import time
 import random
+from mastodon import Mastodon
 from twython import Twython
+
 
 start_delay = random.randint(0,3600)
 time.sleep(start_delay)
@@ -20,6 +22,10 @@ twitter_oauth_token_secret = config['twitter_oauth_token_secret']
 
 twitter = Twython(twitter_app_key, twitter_app_secret, twitter_oauth_token, twitter_oauth_token_secret)
 
+mc = Mastodon(access_token=config['mastodon_token'],
+              api_base_url=config['mastodon_url'])
+
 tweet = choochoogen.maketrain()
 
+mc.toot(tweet)
 twitter.update_status(status=tweet)
