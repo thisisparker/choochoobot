b57d893c3d026f6d4b1893598d6a5c7a8f24ee95
Parker Higgins <parker@parkerhiggins.net>
2024-11-23 16:25:26 -0500

    rip out twitter components

--- choochoopost.py
+++ choochoopost.py
@@ -15,17 +15,7 @@ fullpath = os.path.dirname(os.path.realpath(__file__))
 
 config = yaml.safe_load(open(fullpath + "/config.yaml"))
 
-twitter_app_key = config['twitter_app_key']
-twitter_app_secret = config['twitter_app_secret']
-twitter_oauth_token = config['twitter_oauth_token']
-twitter_oauth_token_secret = config['twitter_oauth_token_secret']
-
-twitter = Twython(twitter_app_key, twitter_app_secret, twitter_oauth_token, twitter_oauth_token_secret)
-
 mc = Mastodon(access_token=config['mastodon_token'],
               api_base_url=config['mastodon_url'])
 
-tweet = choochoogen.maketrain()
-
 mc.toot(tweet)
-twitter.update_status(status=tweet)
